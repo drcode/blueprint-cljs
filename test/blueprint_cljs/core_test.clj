34a9e778e5cbef23ba63dfba2a058f5403273b04
Conrad Barski <lisperati@gmail.com>
2017-02-01 18:31:33 -0600

    initial commit

---
+++ test/blueprint_cljs/core_test.clj
@@ -0,0 +1,7 @@
+(ns blueprint-cljs.core-test
+  (:require [clojure.test :refer :all]
+            [blueprint-cljs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
